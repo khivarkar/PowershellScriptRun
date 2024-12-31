cb894d7a163f01292a15b3c265a178fbcec1fc6a
khivarkar <komal.hivarkar@dataentrega.com>
2024-12-31 13:53:17 +0530

    Delete CreateBlankfile.ps1

--- CreateBlankfile.ps1
+++
@@ -1,9 +0,0 @@
-$filePath = "C:\Temp\BlankFile.txt"
- 
-if (-not (Test-Path -Path (Split-Path -Path $filePath))) {
-    New-Item -ItemType Directory -Path (Split-Path -Path $filePath) | Out-Null
-}
- 
-Set-Content -Path $filePath -Value ""
- 
-Write-Host "Blank text file created at $filePath"
