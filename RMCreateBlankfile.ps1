c6ba09b8c778c6211515f1e14f66a3b099d63711
khivarkar <komal.hivarkar@dataentrega.com>
2025-01-02 15:56:56 +0530

    Add files via upload

---
+++ RMCreateBlankfile.ps1
@@ -0,0 +1,8 @@
+﻿# Define the file path and name
+$filePath = "C:\Tempo\BlankFile.txt"
+ 
+# Create the blank text file (overwrites if it exists)
+Set-Content -Path $filePath -Value ""
+ 
+# Output confirmation
+Write-Host "Blank text file created at $filePath"
