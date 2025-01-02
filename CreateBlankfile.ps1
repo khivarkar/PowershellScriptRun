c149603c070cc3b49db89cf1e1e872ce777f6773
khivarkar <komal.hivarkar@dataentrega.com>
2025-01-02 15:12:40 +0530

    Update CreateBlankfile.ps1

--- CreateBlankfile.ps1
+++ CreateBlankfile.ps1
@@ -1,16 +1,13 @@
-$ScriptContent = @"
-# Define the file path and name
-$filePath = 'C:\Tempo\BlankFile.txt'
-
+﻿# Define the file path and name
+$filePath = "C:\Tempo\BlankFile.txt"
+ 
 # Check if the directory exists; create it if it doesn't
-if (-not (Test-Path -Path (Split-Path -Path $filePath))) { New-Item -ItemType Directory -Path (Split-Path -Path $filePath) | Out-Null }
-
+if (-not (Test-Path -Path (Split-Path -Path $filePath))) {
+    New-Item -ItemType Directory -Path (Split-Path -Path $filePath) | Out-Null
+}
+ 
 # Create the blank text file (or overwrite if it exists)
-Set-Content -Path $filePath -Value ''
-
+Set-Content -Path $filePath -Value ""
+ 
 # Output confirmation
-Write-Host 'Blank text file created at $filePath'
-"@
-
-# Pass as a single string for Start-Process
-Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $($ScriptContent)" -Wait
+Write-Host "Blank text file created at $filePath"
