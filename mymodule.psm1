c611c6c12472bfe62edae97b8a80e51f91aabd60
khivarkar <komal.hivarkar@dataentrega.com>
2025-01-03 14:14:33 +0530

    Add files via upload

---
+++ mymodule.psm1
@@ -0,0 +1,14 @@
+# MyModule.psm1
+
+function Get-Greeting {
+    param (
+        [string]$Name = "User"
+    )
+    "Hello, $Name! Welcome to PowerShell."
+}
+
+function Get-DateInfo {
+    "Today's date is: $(Get-Date -Format 'dddd, MMMM dd, yyyy')"
+}
+
+Export-ModuleMember -Function Get-Greeting, Get-DateInfo
